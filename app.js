2cca6685a88ff7d32387708c9baf47230d92ed20
joshua-anderson <j@zatigo.com>
2014-04-25 16:47:08 -0700

    Move to cluster and socket.io 1.0.x

--- app.js
+++ app.js
@@ -1,71 +1,39 @@
-var configuration = require('./config');
-configuration.configure();
-console .log("Starting Hapsus in " + configuration.config.mode + " mode on port " + configuration.config.port + " at " + (new Date()));
+var cluster = require('cluster');
 
-var users = [];
+if (cluster.isMaster) {
+  configuration = require('./config');
+  configuration.configure();
+  console.log("Starting Hapsus in " + configuration.config.mode + " mode on port " + configuration.config.port + " with pid " + process.pid +  " at " + (new Date()));
 
-var server = require('http').Server(function(req, res) {
-  //This handles the url checking for the server, if it matches the route or the route without a ending slash, display the server info. Example Path= "/game/" this will display server info on "/game" and "/game/"
-  if (require('url').parse(req.url).pathname == configuration.config.path.replace(/\/+$/, '') || require('url').parse(req.url).pathname == configuration.config.path.replace(/\/+$/, '') + "/") {
-    res.writeHead(200, 'application/json');
-    res.end(JSON.stringify({server: "hapsus", name: configuration.config.name, version: require('./package.json').version}));
-  } else {
-    res.writeHead(404, 'text/plain');
-    res.end("404 not found. :'(");
+  var shuttingDown = false;
+
+  for (var i = 0; i < require('os').cpus().length + 1; i++) {
+    cluster.fork();
   }
-});
-var io = require('socket.io').listen(server, {log: configuration.config.socket.io.log });
 
-io.of(configuration.config.path + "socket").on('connection', function(socket){
+  cluster.on('exit', function(worker, code, signal) {
+    // If the worker shuts down themself, let it be as it is gracefully exiting
+    if(worker.suicide) {
+      console.log("Worker " + worker.process.pid+ " shut down successfully");
+    } else { // If the worker dies, create a new one to take its place
+      console.log('Worker ' + worker.process.pid + ' died, attempting to restart');
+      cluster.fork();
+    }
+  });
+
+  // Gracefully allow the workers to disconect when exiting
+  process.on('SIGTERM', gracefulExit).on('SIGINT', gracefulExit);
   
-  socket.set('player', require('./lib/user').add_user(), function() {
-    socket.get('player', function(err, name){
-      if(err) {
-        console.log("Error in Name Retrival(Never Connected?): " + err);
-      } else {
-        socket.emit("connection", {player: name });
-        socket.broadcast.emit("playerSpawn", {player: name}); 
-      }
-    });      
+  process.on('exit', function () {
+    console.log("Hapsus successfully shut down");
   });
+} else {
+  require('./lib/worker');
+}
 
-  //Debug websocket testing (Latentcy)
-  socket.on('ping', function(data){
-    socket.emit("pong", {time_start: data.time_start});
-  });
-
-  socket.on('startMovement', function(data) {
-    socket.get('player', function( err, name) {
-      if(err) {
-        console.log("Err in Name Retrival(Never Connected?): " + err);
-      } else {
-        if(configuration.config.debug.messages) console.log("Start Movement: Moving Left: " +  data.movingLeft + " Angle: " + data.angle);
-        socket.broadcast.emit('moving', {player: name, movingLeft: data.movingLeft, angle: data.angle});
-      }
-    });
-  });
-
-  socket.on('stopMovement', function(data) { 
-    socket.get('player', function(err, name) { 
-      if(err) {
-        console.log("Err in Name Retrival(Never Connected?): " + err);
-      } else {
-        if(configuration.config.debug.messages) console.log("Stop Movement Angle: " + data.angle);
-        socket.broadcast.emit('stop', {player: name, angle: data.angle});
-      }
-    });
-  });
-
-  socket.on('disconnect', function(){
-    socket.get('player', function( err, name) {
-      if(err) {
-        console.log("Err in Name Removal(Never Connected?): " + err);
-      } else {
-        socket.broadcast.emit('playerDisconnect', {player: name});
-        require('./lib/user').remove_user(name);
-      }
-    });
-  });
-});
-
-server.listen(configuration.config.port);
+function gracefulExit() {
+  console.log("Caught SIGTERM or SIGINT, shutting down");
+  for (var id in cluster.workers) {
+    cluster.workers[id].send('DISCONNECT');
+  }
+}
